95344fcbf485ff493bd29bc5099bd5cabd490b22
unset <Giorgio.Antunes@sptech.school>
2024-05-11 22:10:20 -0300

    Funcionalidade de cadastro

---
+++ Banco de Dados/script_projetoHollowKnight.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE projetoIndividualHk;
+
+USE projetoIndividualHk;
+
+CREATE TABLE usuario (
+idUsuario int primary key auto_increment,
+usuario varchar(45),
+email varchar(45),
+cpf char(11),
+senha varchar(45)
+);
+
+CREATE TABLE habilidade (
+idHabilidade int primary key auto_increment,
+nome varchar(45),
+tipo varchar(45),
+numNiveis int
+);
+
+CREATE TABLE inventario (
+idInventario int,
+fkUsuario int,
+fkHabilidade int,
+primary key (idInventario, fkUsuario, fkHabilidade),
+foreign key (fkUsuario) references usuario(idUsuario),
+foreign key (fkHabilidade) references habilidade(idHabilidade),
+nivel int
+);
+
+INSERT INTO habilidade VALUES
+	(default, 'Espírito Vingativo', 'Feitiço', 2),
+	(default, 'Mergulho Desolador', 'Feitiço', 2),
+	(default, 'Espectros Uivantes', 'Feitiço', 2),
+	(default, 'Manto de Asa de Mariposa', 'Mobilidade', 2),
+	(default, 'Garra de Louva-a-Deus', 'Mobilidade', 1),
+	(default, 'Coração de Cristal', 'Mobilidade', 1),
+	(default, 'Asas do Monarca', 'Mobilidade', 1),
+	(default, 'Lágrima de Isma', 'Mobilidade', 1),
+	(default, 'Ferrão dos Sonhos', 'Progressão', 3);
--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -6,7 +6,7 @@ function autenticar(req, res) {
     var senha = req.body.senhaServer;
 
     if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
+        res.status(400).send("Seu email está indefinida!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está indefinida!");
     } else {
@@ -55,8 +55,8 @@ function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
+    var cpf = req.body.cpfServer;
     var senha = req.body.senhaServer;
-    var empresaId = req.body.empresaServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -65,12 +65,10 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (empresaId == undefined) {
-        res.status(400).send("Sua empresa está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, empresaId)
+        usuarioModel.cadastrar(nome, email, cpf, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
