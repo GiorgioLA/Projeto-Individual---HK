e876a9f570b59d0394aeb71e1e277c994f1f0234
unset <Giorgio.Antunes@sptech.school>
2024-05-14 10:31:55 -0300

    tentando acabar funcionalidade de login 1/n

--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -1,5 +1,5 @@
 var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
+// var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
     var email = req.body.emailServer;
@@ -20,20 +20,21 @@ function autenticar(req, res) {
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
+                        // aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
+                            // .then((resultadoAquarios) => {
+                                if (resultadoAutenticar.length > 0) {
                                     res.json({
                                         id: resultadoAutenticar[0].id,
                                         email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
                                         senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
+                                        cpf: resultadoAutenticar[0].cpf,
+                                        // aquarios: resultadoAquarios
                                     });
                                 } else {
                                     res.status(204).json({ aquarios: [] });
                                 }
-                            })
+                
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
--- web-data-viz/src/models/aquarioModel.js
+++ web-data-viz/src/models/aquarioModel.js
@@ -1,23 +1,23 @@
-var database = require("../database/config");
+// var database = require("../database/config");
 
-function buscarAquariosPorEmpresa(empresaId) {
+// function buscarAquariosPorEmpresa(empresaId) {
 
-  var instrucaoSql = `SELECT * FROM aquario a WHERE fk_empresa = ${empresaId}`;
+//   var instrucaoSql = `SELECT * FROM aquario a WHERE fk_empresa = ${empresaId}`;
 
-  console.log("Executando a instrução SQL: \n" + instrucaoSql);
-  return database.executar(instrucaoSql);
-}
+//   console.log("Executando a instrução SQL: \n" + instrucaoSql);
+//   return database.executar(instrucaoSql);
+// }
 
-function cadastrar(empresaId, descricao) {
+// function cadastrar(empresaId, descricao) {
   
-  var instrucaoSql = `INSERT INTO (descricao, fk_empresa) aquario VALUES (${descricao}, ${empresaId})`;
+//   var instrucaoSql = `INSERT INTO (descricao, fk_empresa) aquario VALUES (${descricao}, ${empresaId})`;
 
-  console.log("Executando a instrução SQL: \n" + instrucaoSql);
-  return database.executar(instrucaoSql);
-}
+//   console.log("Executando a instrução SQL: \n" + instrucaoSql);
+//   return database.executar(instrucaoSql);
+// }
 
 
-module.exports = {
-  buscarAquariosPorEmpresa,
-  cadastrar
-}
+// module.exports = {
+//   buscarAquariosPorEmpresa,
+//   cadastrar
+// }
